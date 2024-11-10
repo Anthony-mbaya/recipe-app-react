8985f9d5942549d4d7d3b0071288963fdb5ec5fa
Anthony <anthonymwakulila@gmail.com>
2024-11-10 23:27:22 +0300

    edited the url to use ngrok that allow localhost accessed public

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -147,7 +147,7 @@ const Login = () => {
     }
     try {
       //const token_url = "http://127.0.0.1:8000/api/user/token/";
-      const token_url = "https://randomstring.ngrok.io/api/user/token/";
+      const token_url = "https://5aa3-105-161-86-39.ngrok-free.app/api/user/token/";
       const token_res = await axios.post(token_url, {
         email: email,
         password: password,
@@ -157,7 +157,7 @@ const Login = () => {
       setIsAuth(true);
 
       //const get_url = "http://127.0.0.1:8000/api/user/me/";
-      const get_url = "https://randomstring.ngrok.io/api/user/me/";
+      const get_url = "https://5aa3-105-161-86-39.ngrok-free.app/api/user/me/";
       const get_res = await axios.get(get_url, {
         headers: {
           Authorization: `Token ${token}`,
