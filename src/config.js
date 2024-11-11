e33de847860669072d47d06471bcb489e85091aa
Anthony <anthonymwakulila@gmail.com>
2024-11-11 10:53:28 +0300

    edited the url to use ngrok that allow localhost accessed public

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-
-const BASE_URL = 'https://5fe9-105-161-86-39.ngrok-free.app';
+//const BASE_URL = 'https://5fe9-105-161-86-39.ngrok-free.app';
+const BASE_URL = process.env.REACT_APP_API_URL;
 export default BASE_URL;
 //console.log("BASE_URL:", BASE_URL);
